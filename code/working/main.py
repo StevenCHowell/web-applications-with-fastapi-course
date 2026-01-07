e661ce8b5ef0e45a3de1409f28f97b0e47b617b4
Steven C. Howell <9484229+StevenCHowell@users.noreply.github.com>
2026-01-07 06:59:02 -0500

    organize

--- code/working/main.py
+++ code/working/main.py
@@ -6,12 +6,27 @@ from views import account, home, packages
 
 app = fastapi.FastAPI()
 
-fastapi_chameleon.global_init("templates")
+def main():
+    configure()
+    uvicorn.run(app, host='127.0.0.1', port=8000)
 
-app.include_router(home.router)
-app.include_router(account.router)
-app.include_router(packages.router)
+
+def configure():
+    configure_templates()
+    configure_routes()
+
+
+def configure_templates():
+    fastapi_chameleon.global_init("templates")
+
+
+def configure_routes():
+    app.include_router(home.router)
+    app.include_router(account.router)
+    app.include_router(packages.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    main()
+else:
+    configure()
