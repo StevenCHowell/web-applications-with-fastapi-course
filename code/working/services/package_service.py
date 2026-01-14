5b3e5f3cff5d61f80e3581c1e23c953bc25045a8
Steven C. Howell <9484229+StevenCHowell@users.noreply.github.com>
2026-01-14 17:02:53 -0500

    data placeholders

--- code/working/services/package_service.py
+++ code/working/services/package_service.py
@@ -1,4 +1,8 @@
-from typing import List
+import datetime
+from typing import List, Optional
+
+from data.package import Package
+from data.release import Release
 
 
 def package_count() -> int:
@@ -19,5 +23,18 @@ def latest_releases(limit: int = 5) -> List:
     return packages[:limit]
 
 
-def get_package_by_id(package_name: str):
-    NotImplemented
+def get_package_by_id(package_name: str) -> Optional[Package]:
+    package = Package(
+        package_name,
+        "This is he summary",
+        "Full details here!",
+        "https://fastapi.tiangolo.com/",
+        "MIT",
+        "Sebastian Ramirez",
+    )
+    return package
+
+
+def get_latest_release_for_package(package_name: str) -> Optional[Release]:
+    release = Release("1.42.0", datetime.datetime.now())
+    return release
--- code/working/viewmodels/packages/details_viewmodel.py
+++ code/working/viewmodels/packages/details_viewmodel.py
@@ -10,6 +10,13 @@ class DetailsViewModel(ViewModelBase):
 
         self.package_name = package_name
         self.package = package_service.get_package_by_id(package_name)
+        self.latest_release = package_service.get_latest_release_for_package(
+            package_name
+        )
+        self.is_latest = True
 
-        if not self.package:
+        if not self.package or not self.latest_release:
             return
+
+        self.latest_version = self.latest_release.version
+        self.maintainers = self.package.maintainers
